522c99af5a713f8495f3629dbd8dc82468398ab9
Larry X <papadocarock@gmail.com>
2020-09-23 22:36:32 -0400

    working questions

--- index.js
+++ index.js
@@ -1,73 +1,80 @@
-var inquirer = require("inquirer");
-var fs = require("inquirer");
-const questions = [
-        inquirer
-        .prompt([{
-                type: "input",
-                message: "Title?",
-                name: "Title"
-            },
-            {
-                type: "input",
-                message: "Description?",
-                name: "description"
-            },
-            {
-                type: "input",
-                message: "Table Of Contents",
-                name: "table of contents"
-            },
-            {
-                type: "input",
-                message: "Installation?",
-                name: "install"
-            },
-            {
-                type: "checkbox",
-                message: "License?",
-                choices: ['Apache',
-                    'MIT',
-                    'GNU',
-                    'ISC'
-                ],
-                name: "license"
-            },
-            {
-                type: "input",
-                message: "GitHub username?",
-                name: "username"
-            },
-            {
-                type: "input",
-                message: "Email?",
-                name: "email"
-            },
-            {
-                type: "input",
-                message: "Contributor",
-                name: "contributor"
-            },
-            {
-                type: "input",
-                message: "Test?",
-                name: "test"
-            },
-            {
-                type: "input",
-                message: "Anything Else?",
-                name: "random"
-            }
-        ])
-        .then(generateMarkdown(inquirer) {
-                // function to generate markdown for README
-                function generateMarkdown(inquirer) {
-                    return `
-#${data.title}\n
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = ("generateMarkdown");
+const questions = [];
+inquirer
+    .prompt([{
+            type: "input",
+            message: "Title?",
+            name: "Title"
+        },
+        {
+            type: "input",
+            message: "Description?",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Table Of Contents",
+            name: "toc"
+        },
+        {
+            type: "input",
+            message: "Installation?",
+            name: "install"
+        },
+        {
+            type: "checkbox",
+            message: "License?",
+            choices: ['Apache',
+                'MIT',
+                'GNU',
+                'APM',
+                'GitHub',
+                'CPAN',
+                'ISC'
 
+            ],
+            name: "license"
+        },
+        {
+            type: "input",
+            message: "GitHub username?",
+            name: "username"
+        },
+        {
+            type: "input",
+            message: "Email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Contributor",
+            name: "contributor"
+        },
+        {
+            type: "input",
+            message: "Test?",
+            name: "test"
+        },
+        {
+            type: "input",
+            message: "Anything Else?",
+            name: "random"
+        }
+    ])
+    .then(() => {
+        var ReadMe = generateMarkdown(questions);
+        fs.writeFile("generator.md", ReadMe, err => {
+            if (err) throw err;
+            console.log("Generated ReadMe");
+        });
+    });
+
+// function to initialize program
+function init() {
 
 }
 
-export default generateMarkdown;
-                
-
-        ])
+// function call to initialize program
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,6 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
-  
-  `;
+function generateMarkdown() {
+    return '\n\n#' + data.title + '\n\n##Title\n\n#' + data.description + '\n\n#description\n\n#' + data.toc + '\n\n#toc\n\n#' + data.install + '\n\n#install\n\n#' + data.license + '\n\n#license\n\n#' + data.email + '\n\n#email\n\n#' + data.contributor + '\n\n#contributor\n\n#' + data.test + '\n\n#test\n\n\n#' + data.random + '\n\n#random\n\n';
 }
 
-export default generateMarkdown;
+module.exports = generateMarkdown;
